4ccbd209535bbf9999e9825d4e66f3a94824de0a
2790004z <2790004z@student.gla.ac.uk>
2023-01-29 17:09:15 +0000

    Chapter 5

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tango_with_django_project
+
+Chapter 4: 'about.html' already includes 'cat.jpg' and 'rango,jpg', but tester says I have not.
--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-"""def index(request):
+def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!', 'categories': category_list}
@@ -37,14 +37,4 @@ def show_category(request, category_name_slug):
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
-
-    return render(request, 'rango/category.html', context=context_dict)"""
-
-def index(request):
-    return HttpResponse("Rango says hey there partner!\n <a href='/rango/about/'>About</a>")
-
-
-def about(request):
-    return HttpResponse("Rango says here is the about page.\n <a href='/rango/'>Index</a>")
-
-
+    return render(request, 'rango/category.html', context=context_dict)
