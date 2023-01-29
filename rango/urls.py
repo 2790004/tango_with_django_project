a49ceffb381e36fed695492a7ff6896d85580c2d
2790004z <2790004z@student.gla.ac.uk>
2023-01-29 16:36:12 +0000

    Chapter 3

---
+++ rango/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rango import views
+
+app_name = "Rango"
+
+"""urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category')
+]"""
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+]
